e01a1de0d882eacba9a2823f7d4a9a9463a08fc7
Parang9423 <parang5736@gmail.com>
2026-07-20 19:27:31 +0900

    Show background indexing status in application shell

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,1 +1,11 @@
-export { default } from './AppFolderFirst'
+import AppFolderFirst from './AppFolderFirst'
+import ScanStatusBanner from './ScanStatusBanner'
+
+export default function App() {
+  return (
+    <>
+      <div className="global-scan-status"><ScanStatusBanner /></div>
+      <AppFolderFirst />
+    </>
+  )
+}
