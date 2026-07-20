deb2db30fa65ce6067dc7850341fda17d0a5c6b3
Parang9423 <parang5736@gmail.com>
2026-07-20 15:40:22 +0900

    feat: register dataset folder browser routes

--- apps/api/app/entrypoint.py
+++ apps/api/app/entrypoint.py
@@ -1,9 +1,11 @@
 from .collaboration import ensure_collaboration_schema, router as collaboration_router
 from .collaboration_summary import router as collaboration_summary_router
+from .dataset_browser import router as dataset_browser_router
 from .main import app
 
 app.include_router(collaboration_router)
 app.include_router(collaboration_summary_router)
+app.include_router(dataset_browser_router)
 
 
 @app.on_event("startup")
