3423c03968a5b96ef8659f07aad51378a8d916a1
Parang9423 <parang5736@gmail.com>
2026-07-20 15:36:36 +0900

    feat: register collaborative summary API

--- apps/api/app/entrypoint.py
+++ apps/api/app/entrypoint.py
@@ -1,7 +1,9 @@
 from .collaboration import ensure_collaboration_schema, router as collaboration_router
+from .collaboration_summary import router as collaboration_summary_router
 from .main import app
 
 app.include_router(collaboration_router)
+app.include_router(collaboration_summary_router)
 
 
 @app.on_event("startup")
